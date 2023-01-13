492ea39b349e8f6ed9ea0cb75527c340d7619591
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2023-01-13 02:37:25 +0000

    npx prettier --write .

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,8 +19,6 @@ module MyApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.generators do |g|
-      g.test_framework :rspec
-    end
+    config.generators { |g| g.test_framework :rspec }
   end
 end
